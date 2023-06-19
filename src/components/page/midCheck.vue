5875b1f12bb4c053b5a8f108a3ba6525811c566d
wuziwen <wuziwen@vandream.com>
2023-06-19 18:29:50 +0800

    中期检查页面

--- src/components/page/midCheck.vue
+++ src/components/page/midCheck.vue
@@ -17,69 +17,26 @@
         <div class="cont">
           <div class="options">
             <el-button type="primary" icon="el-icon-search" size="mini" @click="findResult">查询</el-button>
-            <el-button type="primary" icon="el-icon-edit" size="mini" @click="addResult">添加</el-button>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="update">更新</el-button>
             <el-button type="primary" size="mini" @click="upload">上传<i class="el-icon-upload el-icon--right"></i></el-button>
             <el-button type="primary" icon="el-icon-thumb" size="mini" @click="flush">刷新</el-button>
           </div>
-          <!--          文件上传对话框-->
-          <el-dialog :visible.sync="isUpload">
-            <el-upload
-                class="upload-demo"
-                ref="upload"
-                action="http://localhost:8081/score/upload"
-                :on-preview="handlePreview"
-                :on-remove="handleRemove"
-                :on-success="(res)=>uploadSuccess(res)"
-                :auto-upload="false">
-              <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-              <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
-              <div slot="tip" class="el-upload__tip">可上传txt文件</div>
-            </el-upload>
-          </el-dialog>
+
           <!--    嵌套表单的对话框-->
-          <el-dialog :title="isAdd ? '新增成绩信息' : isFind ? '查询成绩信息' : '编辑成绩信息' " :visible.sync="dialogFormVisible">
+          <el-dialog :title="isFind ? '查询中期检查情况' : '更新中期检查情况' " :visible.sync="dialogFormVisible">
             <span class = "span" v-if="!(isAdd || isFind)">该操作只需要修改成绩</span>
             <el-form :model="formResult">
-              <!--        学号-->
-              <el-form-item label="学号" :label-width="formLabelWidth">
-                <el-input v-model="formResult.sid" autocomplete="off"></el-input>
+              <el-form-item label="选题编号" :label-width="formLabelWidth">
+                <el-input v-model="formResult.selectTopicId" autocomplete="off"></el-input>
               </el-form-item>
-              <!--        姓名-->
-              <el-form-item label="姓名" :label-width="formLabelWidth">
-                <el-input v-model="formResult.name" autocomplete="off"></el-input>
+              <el-form-item label="中期检查日期" :label-width="formLabelWidth">
+                <el-date-picker v-model="formResult.date" type="datetime" placeholder="选择日期时间"></el-date-picker>
               </el-form-item>
-              <!--        课程名-->
-              <el-form-item label="课程名" :label-width="formLabelWidth">
-                <el-input v-model="formResult.courseName" autocomplete="off"></el-input>
+              <el-form-item label="中期检查意见" :label-width="formLabelWidth">
+                <el-input v-model="formResult.opinion" autocomplete="off"></el-input>
               </el-form-item>
-              <!--        得分-->
-              <el-form-item label="成绩" :label-width="formLabelWidth" v-if="!isFind">
-                <el-input v-model="formResult.score" autocomplete="off"></el-input>
-              </el-form-item>
-              <!--        专业班级-->
-              <el-form-item label="专业" :label-width="formLabelWidth" v-if="isFind">
-                <el-select v-model="formResult.major" placeholder="请选择专业">
-                  <el-option value="计算机"></el-option>
-                  <el-option value="物联网"></el-option>
-                  <el-option value="电信科"></el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item label="年级" :label-width="formLabelWidth" v-if="isFind">
-                <el-select v-model="formResult.grade" placeholder="请选择年级">
-                  <el-option value="19"></el-option>
-                  <el-option value="20"></el-option>
-                  <el-option value="21"></el-option>
-                  <el-option value="22"></el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item label="班级" :label-width="formLabelWidth" v-if="isFind">
-                <el-select v-model="formResult.classNum" placeholder="请选择班级">
-                  <el-option value="1"></el-option>
-                  <el-option value="2"></el-option>
-                  <el-option value="3"></el-option>
-                  <el-option value="4"></el-option>
-                  <el-option value="5"></el-option>
-                </el-select>
+              <el-form-item label="中期检查地点" :label-width="formLabelWidth">
+                <el-input v-model="formResult.location" autocomplete="off"></el-input>
               </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
@@ -90,14 +47,11 @@
           <div>
             <!--    主体表格部分-->
             <el-table :data="tableData.slice((currentPage -1) * pageSize, currentPage*pageSize)" style="width: 100%">
-              <el-table-column prop="id" label="成绩编号" width="0" v-if="showResultId"></el-table-column>
-              <el-table-column prop="sid" label="学号" width="100"></el-table-column>
-              <el-table-column prop="name" label="姓名" width="100"></el-table-column>
-              <el-table-column prop="score" label="成绩" width="100"></el-table-column>
-              <el-table-column prop="courseName" label="课程" width="100"></el-table-column>
-              <el-table-column prop="major" label="专业" width="100"></el-table-column>
-              <el-table-column prop="grade" label="年级" width="100"></el-table-column>
-              <el-table-column prop="classNum" label="班级" width="100"></el-table-column>
+              <el-table-column prop = "id" label = "学号" width="100"></el-table-column>
+              <el-table-column prop="selectTopicId" label="选题编号" width="100"></el-table-column>
+              <el-table-column prop="date" label="中期检查日期" width="200"></el-table-column>
+              <el-table-column prop="opinion" label="中期检查意见" width="250"></el-table-column>
+              <el-table-column prop="location" label="中期检查地点" width="150"></el-table-column>
               <el-table-column fixed="right" label="操作" width="200">
                 <template slot-scope="scope">
                   <el-button type="primary" icon="el-icon-edit" size="mini" @click="editResult(scope.row)">编辑</el-button>
@@ -157,14 +111,10 @@ export default {
       ],
       formResult: {
         id: '',
-        cid: '',
-        sid: '',
-        name: '',
-        score: '',
-        courseName: '',
-        major: '',
-        grade: '',
-        classNum: ''
+        selectTopicId: '',
+        date: '',
+        opinion: '',
+        location: ''
       },
       dialogFormVisible: false,
       //  分页数据
@@ -190,7 +140,7 @@ export default {
       this.getData()
     },
     getData(){
-      this.service.get('/score')
+      this.service.get('/midterm_check')
           .then(res=>{
             console.log(res)
             if(res.data.code === 200){
@@ -198,7 +148,6 @@ export default {
                 message: '信息加载成功',
                 type: 'success'
               })
-              console.log("请求所有学生信息成功");
               this.tableData = [...res.data.data]
               this.total = this.tableData.length
               console.log(this.tableData.length)
@@ -213,6 +162,11 @@ export default {
           .catch(err=>{
             console.log(err)
           })
+    },
+    update(){
+      this.formResult = {}
+      this.dialogFormVisible = true
+      this.isFind = false
     },
     //新增成绩信息
     addResult(){
@@ -232,36 +186,10 @@ export default {
     },
     //对话框的确认按钮
     confirm(){
-      console.log(this.formResult)
       this.dialogFormVisible = false
-      if(this.isAdd){
-        //新增
-        this.service.post('/score',this.formResult)
+      if(this.isFind){
+        this.service.post('/midterm_check/find', this.formMidtermCheck)
             .then(res=>{
-              console.log(res)
-              if(res.data.code == 200){
-                this.$message({
-                  message: '新增成功',
-                  type: 'success'
-                })
-                this.getData()
-              }else{
-                this.$message({
-                  message: res.data.msg,
-                  type: 'error'
-                })
-              }
-            })
-            .catch(err=>{
-              console.log(err)
-            })
-      }else if(this.isFind){
-        this.formResult.id = '-1'
-        this.formResult.score = '-1'
-        //查询
-        this.service.post('/score/getScore',this.formResult)
-            .then(res=>{
-              console.log(res)
               if(res.data.code == 200){
                 this.$message({
                   message: '查询成功',
@@ -277,22 +205,22 @@ export default {
               }
             })
             .catch(err=>{
-              console.log("这里出错")
               console.log(err)
             })
       }else{
-        //编辑
-        this.service.put('/score',this.formResult)
+        this.service.put('/midterm_check', this.formMidtermCheck)
             .then(res=>{
-              console.log(res)
               if(res.data.code == 200){
                 this.$message({
-                  message: '修改成功',
+                  message: '更新成功',
                   type: 'success'
                 })
                 this.getData()
               }else{
-
+                this.$message({
+                  message: res.data.msg,
+                  type: 'error'
+                })
               }
             })
             .catch(err=>{
